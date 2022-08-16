8b6be077080e1c494634f13f7ee9b7dd54156d66
Ganesha Danu Enastika <ganeshadanu1@gmail.com>
2022-08-16 20:21:25 +0700

    add auto format code

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@ Color ray_color(const Ray& r) {
 }
 
 int main() {
-    // image size 
+    // image size
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width  = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Camera
-    auto viewport_height = 2.0;
+        auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
     auto focal_length = 1.0;
 
@@ -26,7 +26,6 @@ int main() {
     auto vertical = Point3(0, viewport_height, 0);
     auto lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length);
 
-
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
@@ -34,15 +33,10 @@ int main() {
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
             Ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            /* Color color( */
-            /*         double(i) / (image_width - 1), */
-            /*         double(j) / (image_height - 1), */
-            /*         0.25 */
-            /*         ); */
 
             Color color = ray_color(r);
             write_color(std::cout, color);
         }
-    }  
+    }
 }
 
